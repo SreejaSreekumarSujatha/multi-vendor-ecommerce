7a9bd444476d9d0511d3cc8d56821929eba7c4c9
Sreeja Sreekumar Sujatha <sreejasreekumarsujatha@gmail.com>
2025-07-04 22:29:55 -0400

    Create basic project structure and first PHP file

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+// This is the entry point of our application
+// Every request comes through this file
+
+echo "Welcome to Multi-Vendor E-commerce Platform!";
+echo "<br>";
+echo "Today is: " . date('Y-m-d H:i:s');
+?>
